001a96da24147836b9ee77c2331465f1e3e84df2
Belle Xu <bellex22@protonmail.com>
2023-07-26 15:34:51 -0400

    first edit

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import connect_db, db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +14,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.get('/')
+def show_homepage():
+
+    return redirect('/users')
+
+@app.get('/users')
+def show_users():
+
+    return render_template('user_list.html')
+
+@app.get('/users/new')
+def add_user_form():
+
+    return render_template('new_user.html')
+
+
