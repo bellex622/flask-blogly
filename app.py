e5e3ffba11138f562649aa7440fcb5229eb6a5d4
Stuart Fleisher <stufleisher@gmail.com>
2023-07-26 19:09:28 -0400

    updated app.py

--- app.py
+++ app.py
@@ -22,12 +22,35 @@ def show_homepage():
 
 @app.get('/users')
 def show_users():
-
-    return render_template('user_list.html')
+    users = User.query.all()
+    print("\n\n****","users:", users)
+    return render_template('user_list.html',users = users)
 
 @app.get('/users/new')
 def add_user_form():
 
     return render_template('new_user.html')
 
+@app.post('/users/new')
+def handle_add_user_form():
+    """Creates a db entry with form data and redirects"""
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
 
+    if image_url == "":
+        image_url = None
+
+    user = User(first_name=first_name,last_name=last_name,image_url=image_url)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.get('/users/<user_id>')
+def show_user_details(user_id):
+    user = User.query.get(user_id)
+    return render_template(
+        'user_details.html',
+        user=user
+    )
